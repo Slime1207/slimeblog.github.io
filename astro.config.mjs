c1ed04ee907df53c0bf2fd3da3191196edb91a54
SlimeKing # 史莱姆王 <foreverslimeking@gmail.com>
2024-09-16 23:29:35 +0800

    Change defineConfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,8 +31,8 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: "https://slime1207.github.io/",
+  base: "slimeblog.github.io",
   trailingSlash: "always",
   integrations: [
     tailwind(),
